b0eb6db3181c02042ed9e687c487545fe338fff5
Bennett Clews <bennett.clews@gmail.com>
2021-01-13 15:02:28 +1100

    wip

--- working-files/gatsby/gatsby-config.js
+++ working-files/gatsby/gatsby-config.js
@@ -1,10 +1,27 @@
 // Documentation:
 //     https://www.gatsbyjs.com/docs/gatsby-config/
 
+import dotenv from 'dotenv';
+dotenv.config({path: '.env'});
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in Hamilton!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      // projectId taken from manage.sanity.io
+      options: {
+        projectId: 'cstxibs8',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      }
+    },
+  ]
 };
